e49530fdb099e4727473005a385d8d036dbd9ee3
Luka Berukashvili <lukaberukashvili@icloud.com>
2021-11-01 13:58:53 +0400

    Fix: routes and redirect

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "pages/Movies";
 import Shows from "pages/Shows";
 import Favourites from "pages/Favourites";
@@ -9,7 +9,10 @@ const Routes = () => {
   return (
     <>
       <Switch>
-        <Route exact path={["/", "/movies"]} component={Movies} />
+        <Route exact path='/'>
+          <Redirect from='/' to='/movies'></Redirect>
+        </Route>
+        <Route path='/movies' component={Movies} />
         <Route path='/shows' component={Shows} />
         <Route path='/favourites' component={Favourites} />
         <Route path='/details/:id' component={Details} />
